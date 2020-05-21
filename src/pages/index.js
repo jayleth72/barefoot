effda3f9acd9322f71636a7130ec24b3fe3c60e5
Jay Letheby <jay.letheby@gmail.com>
2020-05-21 22:07:25 +1000

    added background image optimisations for homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,37 @@
 import React from "react"
 import Layout from "../components/Layout"
-import Simplehero from "../components/SimpleHero"
+import StyledHero from "../components/StyledHero"
 import Banner from "../components/Banner"
 import { Link } from "gatsby"
 import About from "../components/home/About"
 import Services from "../components/home/Services"
+import { graphql } from "gatsby"
 
-export default () => (
+export default ({ data }) => (
   <Layout>
-    <Simplehero>
+    <StyledHero home="true" img={data.defaultBcg.childImageSharp.fluid}>
       <Banner
         title="Be Inspired"
         info="Creative and applied learning support for homeschooling"
       >
         <Link to="/about" className="btn-white">
-          Learn More
+          Learn how to crank
         </Link>
       </Banner>
-    </Simplehero>
+    </StyledHero>
     <About />
     <Services />
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    defaultBcg: file(relativePath: { eq: "defaultBcg.jpeg" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4160) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
