4fa1ed99b7dfdc5cad1a856cdfecdfdc61764a94
Jay Letheby <jay.letheby@gmail.com>
2021-02-04 17:01:41 +1000

    test rf

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -40,7 +40,7 @@ const About = (props) => {
             nurturing Community so they feel hope of "Becoming" a valued,
             influential and passionate member of our Society.
           </p>
-		  console.log("Show read more in About = " showReadMore)
+		  console.log("fucking cuntfuck")
 		  
 		  <ReadMoreButton showReadMore = {showReadMore} />
 		 
