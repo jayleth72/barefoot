5353d4481ea3ba7eea68d44c9ec84c25436859d2
Jay Letheby <jay.letheby@gmail.com>
2021-02-04 15:58:35 +1000

    test 13

--- src/components/ReadMoreButton.js
+++ src/components/ReadMoreButton.js
@@ -2,6 +2,7 @@ import React from "react"
 
 const ReadMoreButton = props => {
   let { showReadMore } = props
+  console.log(showReadMore)
   if (showReadMore) {
     return (
           	<button type="button" className="btn-primary">
