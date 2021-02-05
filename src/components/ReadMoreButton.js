372bebf4d8a254e2f11ff4a8e38cdfe50bacadcf
Jay Letheby <jay.letheby@gmail.com>
2021-02-05 13:11:38 +1000

    tttt

--- src/components/ReadMoreButton.js
+++ src/components/ReadMoreButton.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react"
 
 const ReadMoreButton = props => {
-  let { showReadMore } = useState(props)
+  let { showReadMore } = props
   console.log(showReadMore) 
   if (showReadMore) {
-	 
+	 console.log("butthole")
     return (
           	<button type="button" className="btn-primary">
             	read more
